bd0f55feca55ec673b302f489a997c5aeb54caec
rosonsunuwar <roshansunuwar70@gmail.com>
2024-02-29 11:18:02 +0545

    recheck

--- store/admin.py
+++ store/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
 from store.models import *
 # Register your models here.
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [OrderItemInline]
+
 admin.site.register(Customer)
 admin.site.register(Product)
-admin.site.register(Kurtha)
-admin.site.register(Saree)
-admin.site.register(Tshirt)
-admin.site.register(Pant)
-admin.site.register(Order)
+# admin.site.register(Kurtha)
+# admin.site.register(Saree)
+# admin.site.register(Tshirt)
+# admin.site.register(Pant)
+admin.site.register(Order, OrderAdmin)
 admin.site.register(OrderItem)
 admin.site.register(ShippingAddress)
--- store/models.py
+++ store/models.py
@@ -122,6 +122,7 @@ class OrderItem(models.Model):
     
     
 class ShippingAddress(models.Model):
+    
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
     address = models.CharField(max_length=200, null=True)
